c46c85c85063156e12135603fe97733eeda01f74
euclidiac <77080451+euclidiac@users.noreply.github.com>
2025-12-26 03:13:01 -0700

    Change

--- Landmarks/Views/LandmarkDetail.swift
+++ Landmarks/Views/LandmarkDetail.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
-    var landmjark: Landmark
+    var landmark: Landmark
     var body: some View {
         ScrollView {
             MapView(coordinates: landmark.locationCoordinate)
